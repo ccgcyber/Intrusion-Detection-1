baa790dd774a9869c909dee4d528e93a67cf6524
jitendra554 <9720jeetu@gmail.com>
2017-09-18 16:00:35 +0530

    corrected cross vaL

--- cross_val.py
+++ cross_val.py
@@ -10,14 +10,13 @@ from sklearn.model_selection import cross_val_score
 
 class Cross_Valid: 
 	def __init__(self,model,x_data,y_data,params=None): 
-      self.model=model  
+     	self.model=model  
 		self.x_data = x_data
 		self.y_data = y_data
 		self.params=params
 
 
 	def cross_val(self,x_train,y_train,params=None):
-		if params==None:
-			params={'cv':5}
-		scores = cross_val_score(self.model, .data, iris.target, cv=5)
+				scores = cross_val_score(self.model,self.x_train, self.y_train, cv=params)
+				#when none it will calculate 3 fold
 		return scores
