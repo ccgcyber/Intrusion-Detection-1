0d4c9301ee4e23733b08ac4ace1ffb36d9cb58ea
jitendra554 <9720jeetu@gmail.com>
2017-09-19 16:28:45 +0530

    cross val checked working properly

--- cross_val.py
+++ cross_val.py
@@ -6,18 +6,40 @@ Created on Mon Sep 18 14:30:45 2017
 """
 
 from sklearn.model_selection import cross_val_score
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+
+def cross_val(self,model,x_train,y_train,c_fold=None):
+	scores = cross_val_score(self.model,self.x_train, self.y_train, cv=c_fold)
+	print('cross validation scores %.3f +/- %.3f ' %(scores.mean(),scores.std()))
+	#when cv= none it will calculate 3 fold
+	return scores
 
 
-class Cross_Valid: 
-	def __init__(self,model,x_data,y_data,params=None): 
-     	self.model=model  
-		self.x_data = x_data
-		self.y_data = y_data
-		self.params=params
+def k_fold_crossval(model,x_train,y_train,K=None):
+	if K==None:
+		K=3
+	f1score=[]   
+	kf = KFold(n_splits = K, shuffle = True)
+	for result in kf.split(y_train):
+		X_train,Y_train= x_train.ix[result[0]],y_train.ix[result[0]]
+		X_test,Y_test = x_train.ix[result[1]],y_train.ix[result[1]]
+		   
+		model.fit(X_train,Y_train)    
+		Y_pred=model.predict(X_test)
+		cnf_matrix = confusion_matrix(Y_test, Y_pred)
+		f1= f1_score(Y_test, Y_pred)
+		f1score.append(f1)
+	return f1score
+	
 
 
-	def cross_val(self,x_train,y_train,cv=None):
-		scores = cross_val_score(self.model,self.x_train, self.y_train, cv)
-		print('cross validation scores %.3f +/- %.3f ' %(scores.mean(),scores.std()))
-		#when cv= none it will calculate 3 fold
-		return scores
+
+            
+	
+
+
+
+
