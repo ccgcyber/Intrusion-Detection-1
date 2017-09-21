d4d5898e7adae6b59559d796efc37177c2656f4a
anuragvij264 <anurag@tatrasdata.com>
2017-09-21 13:00:16 +0530

    binarize done

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ root path and returns x_train,y_train,x_test,y_test.
 
 pathname is the path to the root directory of the project 
 """
-def fetch_data(pathname,remove_duplicates=False):
+def fetch_data(pathname,remove_duplicates=False,binary=True):
 	os.chdir(os.path.join(pathname,'data'))
 	df_train = pd.read_csv('session_1_data_train.csv')
 	df_test = pd.read_csv('session_1_data_test.csv')
@@ -23,7 +23,7 @@ def fetch_data(pathname,remove_duplicates=False):
 	y_test = df_test.loc[:,labelname]
 	x_test = df_test.drop(labelname,axis=1,inplace=False)
 	assert x_test.shape,(311028,42)
-
+       
 	if remove_duplicates == True:
 		temp_train=x_train.copy()
 		x_train=x_train[pd.DataFrame(~np.array( temp_train.duplicated()))[0]]
@@ -34,6 +34,9 @@ def fetch_data(pathname,remove_duplicates=False):
 		print('Datasets loaded :)' )
 		print ('The dimensions of the training dataset is {}'.format(x_train.shape))
 		print('The dimensions of test dataset is {}'.format(x_test.shape))
+		if binary == True:
+			y_train=y_train.apply(lambda x: binarize(x))
+
 
 		return x_train,y_train,x_test,y_test
 
@@ -41,6 +44,8 @@ def fetch_data(pathname,remove_duplicates=False):
 		print('Datasets loaded :)' )
 		print ('The dimensions of the training dataset is {}'.format(x_train.shape))
 		print('The dimensions of test dataset is {}'.format(x_test.shape))
+		if binary == True:
+			y_train=y_train.apply(lambda x: binarize(x))
 		return x_train,y_train,x_test,y_test
 
 
@@ -87,9 +92,15 @@ def feature_engineering(x_train,do_normalization=True):
 		#normalize continous features
 		return pd.DataFrame(norm_x_train,columns=cont_features)
 
-
 	else:
 		return x_train 
+
+def binarize(x): 
+	if x =='normal.'
+		return 0 
+
+	else:
+		return 1
 
 
 
