926fe399d33b0c485de661b6bd4f020d1828f007
anuragvij264 <anurag@tatrasdata.com>
2017-09-24 20:18:13 +0530

    print in cv

--- cross_val.py
+++ cross_val.py
@@ -5,7 +5,8 @@ import numpy as np
 def k_fold_crossval(model,x_train,y_train,K=None):
 	if K==None:
 		K=3
-	f1score=[]   
+	f1score=[] 
+	counter=1  
 	kf = KFold(n_splits = K, shuffle = True)
 	for result in kf.split(y_train):
 		X_train,Y_train= x_train.iloc[result[0]],y_train.iloc[result[0]]
@@ -14,7 +15,9 @@ def k_fold_crossval(model,x_train,y_train,K=None):
 		model.fit(X_train,Y_train)    
 		Y_pred=model.predict(X_test)
 		f1= f1_score(Y_test, Y_pred)
+		print('f1 score for {} iteration................. {:.4f}'.format(counter,f1))
 		f1score.append(f1)
+		counter+=1
 	return np.mean(f1score)
 	
 
