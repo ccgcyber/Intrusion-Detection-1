5f41d434243f1c91228b2e72a7ac0ae7dc2df5ef
anuragvij264 <anurag@tatrasdata.com>
2017-09-24 20:24:50 +0530

    error handling to be done for pickle

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,jsonify,request 
+from sklearn.externals import joblib
+import reformat
+import pickle
+
+
+app=Flask(__name__)
+
+
+@app.route('/api',methods=['POST'])
+def predict():
+	json=request.get_json()
+	temp_df=reformat_json(json)
+
+
+	predictions=model.predict(temp_df)
+
+
+	return jsonify(predictions)
+
+
+if __name__=='__main__':
+	model=pickle.load(open('rf_test.pkl','rb'))
+	app.run(port = 2345,debug= True)
