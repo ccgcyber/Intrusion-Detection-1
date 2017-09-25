be6864ffe6d1e97c6093a83108c25c88f258b17c
anurag-shl <anurag@siliconharborlabs.com>
2017-09-25 20:02:07 +0530

    reformat.py

--- reformat.py
+++ reformat.py
@@ -1,42 +1,31 @@
 import json 
 import pandas as pd 
+from data import feature_engineering
 """
 Test json needs to be changed as the feature space is the same as the 
-training data.
-
+training data+
 converts the test json into a modified test dataframe.
 
 """
 
-def reformat_json(json):
-	
+def reformat_json(data):
+	df_sample=pd.read_csv('df_sample.csv')
+	df_sample=df_sample.drop(['Unnamed: 0'],axis=1,inplace=False)
+	test_df=pd.DataFrame(json.loads(data),index=[0])
+	# print (test_df)
 
-	def bin_service(x):
-		if x in {'http','private','smtp','domain_u','other','ftp_data'}:
-			return x
-		else:
-			return 'others'
+	df_test=pd.concat([test_df,df_sample])
+	df_test.reset_index()
+	print (df_sample)
 
-	def bin_flag(x):
-		if x in {'SF','S0','REJ'}:
-			return x
-		else:
-			return 'others'
+	# df_test.drop(['index','level_0'],axis=1,inplace=False)
 
-		dict_temp=json.loads(json)
-		df_temp=pd.DataFrame(dict_temp,index=[0])
+	df_test=feature_engineering(df_test,do_normalization=False)
+	# df_test=df_test.drop(['Unnamed: 0'],axis=1,inplace=False)
 
-		df_temp['service']=df_temp['service'].apply(lambda x: bin_service(x))
-		df_temp=pd.get_dummies(df_temp,columns=['service'],prefix='service',drop_first=True)
-		
-		df_temp['flag']=df_temp['flag'].apply(lambda x: bin_flag(x))
-		df_temp=pd.get_dummies(df_temp,columns=['flag'],prefix='flag',drop_first=True)
-
-		features_to_be_removed=['num_root','su_attempted','num_outbound_cmds','is_host_login']
-		df_temp.drop(features_to_be_removed,axis=1,inplace=True)
+	return df_test
 
 
-		return df_temp 
 
 
 
