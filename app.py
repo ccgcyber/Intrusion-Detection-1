67f5837b93d977da91106f3df32b07f31b5d5611
anurag-shl <anurag@siliconharborlabs.com>
2017-09-25 18:56:19 +0530

    api working

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 
 from flask import Flask,jsonify,request 
 from sklearn.externals import joblib
-import reformat
-
-
+from reformat import reformat_json
+import json
 app=Flask(__name__)
+
+
+
+@app.route('/hello',methods=['GET'])
+def something():
+	return ('Hey There')
 
 @app.route('/api',methods=['POST'])
 def predict_new():
+	print ('Hello World')
+	data =json.dumps((request.json))
 
-	json=request.get_json()
-	temp_df=reformat_json(json)
+	
+	
+	temp_df=reformat_json(data)
+	# print (temp_df)
+
 
 
 	predictions=model.predict(temp_df)
+	# print('predictions ={}'.format(predictions[0]))
+	
 
 
-	return jsonify(predictions)
-
+	return predictions
 
 if __name__=='__main__':
 
-	model=pickle.load(open('rf_test.pkl','rb'))
-	app.run(port = 2345,debug= True)
+	model=joblib.load(open('randomforest.pkl','rb'))
+	app.run(port = 9999,debug= True)
 
 
 
