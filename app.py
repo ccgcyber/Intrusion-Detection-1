e1c433fc08f9b8dd3eabb7a7c51bb1a0ccddbe09
anurag-shl <anurag@siliconharborlabs.com>
2017-09-26 15:42:47 +0530

    some more

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app=Flask(__name__)
 
 @app.route('/hello',methods=['GET'])
 def something():
-	return ('Hey There')
+	return ('Hey There!We got you')
 
 @app.route('/api/bin',methods=['POST'])
 def predict_bin():
@@ -18,8 +18,14 @@ def predict_bin():
 	temp_df=reformat_json(data)
 	# print (temp_df)
 	predictions=model.predict(temp_df)
+
+	if predictions[0]==0:
+		return 'predictions = normal.'
+
+	else:
+		return 'predictions = attack.'
 	
-	return 'predictions={}'.format(predictions[0])
+
 
 @app.route('/api/mlt',methods=['POST'])
 def predict_multi():
@@ -29,13 +35,10 @@ def predict_multi():
 	# print (temp_df)
 	predictions=model_multi.predict(temp_df)
 	
-	return 'predictions={}'.format(predictions[0])
+	return 'predictions = {}'.format(predictions[0])
 
 if __name__=='__main__':
 
 	model=joblib.load(open('randomforest.pkl','rb'))
 	model_multi=joblib.load(open('multirandforest.pkl','rb'))
 	app.run(host="0.0.0.0",port = 9999,debug= True)
-
-
-
