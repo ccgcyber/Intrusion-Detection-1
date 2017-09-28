1bf4fdab418c0398a23da8b2e227454a80a6f403
anuragvij264 <anurag@tatrasdata.com>
2017-09-28 14:43:11 +0530

    some thing removed

--- script.py
+++
@@ -1,9 +0,0 @@
-import os
-from data import fetch_data
-a,b,c,d = fetch_data('',remove_duplicates=False,binary=True)
-from data import feature_engineering
-x = feature_engineering(a,do_normalization=True)
-os.chdir("../")
-from aug import Sampling
-s = Sampling(x,b)
-new_data = s.Smote_Over_Samp(x,b)
